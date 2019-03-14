f0178c8e4696be0134ea2f75aae7d63543d1dce8
Duoliveira <48107078+Duoliveira@users.noreply.github.com>
2019-03-14 18:57:14 -0300

    Update classe Coordenada.java
    criação básica da classe coordenada

--- Coordenada.java
+++ Coordenada.java
@@ -1,4 +1,33 @@
 public class Coordenada 
 {
     private int linha, coluna;
+    
+    public void setLinha(int linha) 
+    {
+    	this.linha = linha;
+    }
+    
+    public void setColuna(int coluna) 
+    {
+    	this.coluna = coluna;
+    }
+    
+    public int getLinha()
+    {
+    	return this.linha;
+    }
+    
+    public int getColuna()
+    {
+    	return this.coluna;
+    }
+    
+    public String toString()
+    {
+    	return "(" +
+    			this.linha + 
+    			"," +
+    			this.coluna +
+    			")";
+    }
 }
