ad832eec459f01990099c8b6035b25dfa782c89f
Matheus Andrey Rodrigues <matheus.randrey@gmail.com>
2019-05-01 11:52:35 -0300

    musicas novas

--- ProjetoMusica/ProjetoMusica/Client/src/Client/Client.java
+++ ProjetoMusica/ProjetoMusica/Client/src/Client/Client.java
@@ -4,14 +4,7 @@ public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		try {
-			Teclado.getUmString();
-		}
-		catch(Exception ex)
-		{
-			
-		}
+
 	}
 
 }
--- ProjetoMusica/ProjetoMusica/src/Server/Server.java
+++ ProjetoMusica/ProjetoMusica/src/Server/Server.java
@@ -4,7 +4,13 @@ public class Server {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			Teclado.getUmString();
+		}
+		catch(Exception ex)
+		{
+			
+		}
 	}
 
 }
